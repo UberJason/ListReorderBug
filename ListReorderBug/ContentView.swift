6da8adc689b81c98b3458f31da148011ddc486d8
Jason Ji <uberjason@gmail.com>
2021-06-14 11:03:51 -0400

    Initial Commit

---
+++ ListReorderBug/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ListReorderBug
+//
+//  Created by Jason Ji on 6/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ListReorderBug/ListReorderBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListReorderBugApp.swift
+//  ListReorderBug
+//
+//  Created by Jason Ji on 6/14/21.
+//
+
+import SwiftUI
+
+@main
+struct ListReorderBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
